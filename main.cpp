71663f97fc53c3e4db1e27b3872b12794205b584
Max Wiesenfeld <maw5859@utexas.edu>
2023-11-29 04:31:47 -0600

    Began write-out of full runner through ant moves

--- main.cpp
+++ main.cpp
@@ -1,35 +1,62 @@
 #include <vector>
 #include <functional>
+#include "memory"
+#include "ant.hpp"
+#include "marker.hpp"
+#include "obstacle.hpp"
 #include "visualize.hpp"
+
 
 using std::function;
 using std::vector;
 
 int main() {
-  Obstacle test = Obstacle(10.0, 10.0, WALL,
-			   [](float dx, float dy) -> bool {
-			     if (dx*dx + dy*dy <= 20) { return true; }
-			     return false;
-			   });
+    int nants = 10;
 
-  Obstacle test2 = Obstacle(5.0, 5.0, FOOD,
-			    [](float dx, float dy) -> bool {
-			      if (std::abs(dx) <= 2.5 && std::abs(dy) <= 1) { return true; }
-			      return false;
-			    });
+    CircleObstacle nest = CircleObstacle(5, 5, ObstacleType(NEST), 2);
+    CircleObstacle food = CircleObstacle(15, 15, ObstacleType(FOOD), 3);
+    int nfood = 30;
 
-  Obstacle test3 = Obstacle(20.0, 20.0, NEST,
-			    [](float dx, float dy) -> bool {
-			     if (dx*dx + dy*dy <= 4) { return true; }
-			     return false;
-			   });
+    vector<std::shared_ptr<Obstacle>> walls = {std::make_shared<Obstacle>(8, 5, ObstacleType(WALL), 2, 4)};
 
-  vector<Obstacle> obs(3);
-  obs.at(0) = test;
-  obs.at(1) = test2;
-  obs.at(2) = test3;
+    std::shared_ptr<std::default_random_engine> engine = std::make_shared<std::default_random_engine>();
+    vector<std::shared_ptr<Ant>> ants;
+    for (int i = 0; i < nants; i++) {
+        ants.push_back(std::make_shared<Ant>(engine, nest));
+    }
 
-  Window window = Window(25,25,obs);
-  window.initializeObstacles();
-  window.draw();
+    vector<std::shared_ptr<Marker>> markers;
+
+    while (nfood > 0) {
+        for (auto ant: ants) {
+            ant->move(food, nest, walls, markers, 3);
+        }
+    }
+
+//    Obstacle test = Obstacle(10.0, 10.0, WALL,
+//                             [](float dx, float dy) -> bool {
+//                                 if (dx * dx + dy * dy <= 20) { return true; }
+//                                 return false;
+//                             });
+//
+//    Obstacle test2 = Obstacle(5.0, 5.0, FOOD,
+//                              [](float dx, float dy) -> bool {
+//                                  if (std::abs(dx) <= 2.5 && std::abs(dy) <= 1) { return true; }
+//                                  return false;
+//                              });
+//
+//    Obstacle test3 = Obstacle(20.0, 20.0, NEST,
+//                              [](float dx, float dy) -> bool {
+//                                  if (dx * dx + dy * dy <= 4) { return true; }
+//                                  return false;
+//                              });
+//
+//    vector<Obstacle> obs(3);
+//    obs.at(0) = test;
+//    obs.at(1) = test2;
+//    obs.at(2) = test3;
+
+    Window window = Window(25, 25, obs);
+    window.initializeObstacles();
+    window.draw();
 }
