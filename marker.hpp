d297cf77eaa1d9ed83d0648ff7099e4d84e8578b
Max Wiesenfeld <maw5859@utexas.edu>
2023-12-05 23:56:13 -0600

    Added minor helper functions

--- marker.hpp
+++ marker.hpp
@@ -12,6 +12,8 @@ class Marker {
         bool is_food() { return food; }
         int get_span() { return span; }
         void decrement_span() { span--; }
+        float getX() { return x; }
+        float getY() { return y; }
         bool in_fov(function<bool(float, float)> const &fov_func) { return fov_func(x, y); }
         float distance(float x_in, float y_in) { return std::sqrt((x - x_in) * (x - x_in) + (y - y_in) * (y - y_in)); }
 };
